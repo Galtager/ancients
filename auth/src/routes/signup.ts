741533b2741b0b81ccccc3e4e89868fd4bf38172
gal tager <gal@alterno.io>
2024-03-30 18:43:50 +0300

    feat: add auth routes

---
+++ auth/src/routes/signup.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from 'express'
+import { body, validationResult } from 'express-validator';
+const router = express.Router()
+
+const validators = [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password').trim().isLength({ min: 4, max: 20 }).withMessage('Password must be between 4 and 20 characters')
+]
+
+router.post('/api/users/signup', validators, (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).send(errors.array())
+    }
+    const { email, password } = req.body;
+    console.log('creating a userr')
+    res.send({})
+})
+
+export default router  
