95521241ee084a8b164ab0bb5913981ea36672e0
gal tager <gal@alterno.io>
2024-04-30 19:51:19 +0300

    feat: add submit http request

--- client/pages/signup.js
+++ client/pages/signup.js
@@ -4,16 +4,20 @@ const Signup = () => {
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-
+    const [errors, setErrors] = useState([])
     const onSubmit = async (event) => {
         event.preventDefault();
-        await axios.post("/api/users/signup", { email, password })
-        console.log(email, password)
+        try {
+            const response = await axios.post("/api/users/signup", { email, password })
+            console.log(response.data)
+        } catch (error) {
+            setErrors(error.response.data.errors)
+        }
     }
 
     return (
         <div className="flex w-full justify-center h-full absolute bg-gray-200">
-            <div className="w-full max-w-xs m-auto">
+            <div className="w-full max-w-md m-auto">
                 <form onSubmit={onSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
                     <div className="mb-4">
                         <label className="block text-gray-700 text-sm font-bold mb-2" >
@@ -38,6 +42,14 @@ const Signup = () => {
                             id="password"
                             placeholder="***********" />
                     </div>
+                    {errors.length && <div class="bg-red-50 p-2 mb-4">
+                        <p class=" py-4 text-red-900 font-semibold text-lg">Ooops...</p>
+                        <ul class="px-6  list-disc">
+                            {errors.map(error => (
+                                <li key={error.message} className="text-md text-red-900 text-sm">{error.message}</li>
+                            ))}
+                        </ul>
+                    </div>}
                     <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" >
                         Sign In
                     </button>
