47b92f7f4eede12e8418754288b80a0856767bb2
gal tager <gal@alterno.io>
2024-05-02 11:00:07 +0300

    feat: config

---
+++ common/src/index.ts
@@ -0,0 +1,14 @@
+interface Color {
+    red: number;
+    green: number;
+    blue: number;
+}
+
+const color: Color = {
+    red: 10,
+    blue: 20,
+    green: 10
+}
+
+console.log(color)
+export default color
